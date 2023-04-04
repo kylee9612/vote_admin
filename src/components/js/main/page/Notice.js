18df12f8181798ae8d3119c5da052f32e4610e86
sank2168 <sank2168@axiasoft.com>
2023-04-04 18:27:49 +0900

    [L] notice list 추가작업 중간 커밋

---
+++ src/components/js/main/page/Notice.js
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from "react";
+/** CSS **/
+import './NoticeList.css';
+/** Component **/
+import Modal from "../Util/Modal";
+import axios from "axios";
+
+function Notice({notice, searchText, searchType}) {
+    let noticeList = "";
+    let returnMap = "";
+    /**open Detail**/
+    const [modalVisibleId, setModalVisibleId] = useState("")
+    // const [id , setId] = useState();
+    const onModalHandler = id => {
+        // setId(e);
+        console.log(id)
+        const url = "/api/notice/increaseViews"
+        const params = {
+            "nt_no" : id
+        }
+        axios.post(url,params);
+        setModalVisibleId(id)
+    }
+    const onCloseHandler = () => {
+        setModalVisibleId("")
+    }
+
+    /**open Detail**/
+    return (
+        <tr key={notice.nt_no} className={"detail_" + notice.nt_no}>
+            <td onClick={() => onModalHandler(notice.nt_no)}>{notice.nt_no}</td>
+            <td onClick={() => onModalHandler(notice.nt_no)}>{notice.nt_title}</td>
+            <td onClick={() => onModalHandler(notice.nt_no)}
+                style={{textAlign: 'center'}}>{notice.nt_views}</td>
+            <td onClick={() => onModalHandler(notice.nt_no)}>{notice.reg_date}</td>
+
+            <Modal id={notice.nt_no}
+                   modalVisibleId={modalVisibleId}
+                   setModalVisibleId={onCloseHandler}
+                   header={notice.nt_title}
+                   contents={notice.nt_contents}
+                   date={notice.reg_date}
+                   count={notice.nt_views}
+            />
+        </tr>
+    )
+}
+
+export default Notice;
+
