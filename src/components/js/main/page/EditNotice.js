75f1fdf0c0e95e945db70a2c9773275e72e5b180
sank2168 <sank2168@axiasoft.com>
2023-04-11 16:25:43 +0900

    [L] notice 등록 수정

--- src/components/js/main/page/EditNotice.js
+++ src/components/js/main/page/EditNotice.js
@@ -1,53 +1,73 @@
-import {useNavigate} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
+import error from "../../side/Error";
 
-const EditNotice = (type , id) => {
+const EditNotice = (props) => {
+    const id = useParams();
+    const [title, setTitle] = useState("");
+    const [text, setText] = useState("");
+
+    const onChangeTitle = (e) => {setTitle(e.target.value)}
     let history = useNavigate();
-
     useEffect(() => {
+
+        console.log(" id.id : " +id.id);
+        const nt_no = id.id
         let notice;
         const params = {
-            "nt_no": id
+            "nt_no": nt_no
         }
-        axios.get("/api/notice/getNotice", {params})
+        axios.post("/api/notice/getNotice", params)
             .then((response) => {
+                const notice = response.data.data.notice;
+                console.log("response :: " + JSON.stringify(notice.nt_title))
+                console.log("response :: " + notice.nt_title)
 
+                setTitle(notice.nt_title)
+                setText(notice.nt_contents);
             }).catch((error) => {
-
+                console.log("error :: " + error)
         })
     }, [])
 
 
-    function editNotice(type, id) {
-        console.log(type);
+    function editNotice(id) {
+        const url = "/api/notice/editNotice"
+        const params = {
+            "nt_no" : id ,
+            "nt_title" : title ,
+            "nt_contents" : text
+        };
 
+        axios.post(url,params)
+            .then((response) =>{
+                console.log(JSON.stringify(response));
+                alert(response.data.message);
 
-        let url = "";
-        let params = [];
-
-        if (type == "insert") {
-            console.log("insert");
-        } else {
-            console.log("update");
-        }
+            }).catch((error)=>{
+                console.log(JSON.stringify(error));
+                alert(JSON.stringify(error.data.message));
+            }).finally((e)=>{
+            history(-1)
+            })
     }
 
     return <>
         <div className={"editNotice_wrap"}>
             <div className={"editNotice_head_wrap"}>
                 <label htmlFor={"title"} >제목</label>
-                <input id="title" type={"text"}/>
+                <input id="title" type={"text"} value={title} onChange={onChangeTitle}/>
             </div>
             <div className={"editNotice_contents_wrap"}>
                 <label></label>
-                <ReactQuill></ReactQuill>
+                <ReactQuill value={text} onChange={setText}></ReactQuill>
             </div>
         </div>
         <div className={"editNotice_footer"}>
-            <button onClick={() => editNotice(type, id)}>등록하기</button>
+            <button onClick={() => editNotice(id.id)}>등록하기</button>
             <button onClick={() => history(-1)}>뒤로가기</button>
         </div>
     </>
